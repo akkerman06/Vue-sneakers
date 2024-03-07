8d83282fa979b45a560c21216a02e980f01a08fa
akkerman06 <islamhaluhaev225@gmail.com>
2024-03-07 21:51:52 +0300

    drawer drawerHead addtofav... fetchfavor.. sortBy searchQuery

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,5 +1,6 @@
 <script setup>
 defineProps({
+  id: Number,
   imageUrl: String,
   title: String,
   price: Number,
@@ -11,27 +12,26 @@ defineProps({
 </script>
 
 <template>
-  <div class="">
-    <div
-      class="bg-white relative m-10 border border-slate-100 rounded-3xl p-8 cursor-pointer transition hover:-translate-y-2 hover:shadow-xl"
-    >
-      <img
-        :src="!isFavorite ? '/like-1.svg' : '/like-2.svg'"
-        alt="Like 1"
-        class="absolute top-8 left-8"
-        @click="onClickFavorite"
-      />
-      <img :src="imageUrl" alt="Sneaker" />
-      <p class="mt-2">{{ title }}/p></p>
+  <div
+    class="relative flex flex-col w-full border border-slate-100 rounded-xl p-8 cursor-pointer transition hover:shadow-xl hover:transform hover:-translate-y-2"
+  >
+    <img
+      v-if="onClickFavorite"
+      :src="!isFavorite ? '/like-1.svg' : '/like-2.svg'"
+      alt="Like 1"
+      class="absolute top-8 left-8"
+      @click="onClickFavorite"
+    />
+    <img class="w-full" :src="imageUrl" alt="Sneaker" />
+    <p class="mt-2">{{ title }}</p>
 
-      <div class="flex justify-between mt-5">
-        <div class="flex flex-col">
-          <span class="text-slate-400">Цена:</span>
-          <b>1205 руб.</b>
-        </div>
-
-        <img @click="onCliclAdd" :src="!isAdded ? '/plus.svg' : '/checked.svg'" alt="Plus" />
+    <div class="flex justify-between mt-5">
+      <div class="flex flex-col">
+        <span class="text-slate-400">Цена:</span>
+        <b>{{ price }}</b>
       </div>
+
+      <img @click="onCliclAdd" :src="!isAdded ? '/plus.svg' : '/checked.svg'" alt="Plus" />
     </div>
   </div>
 </template>
--- src/components/CardList.vue
+++ src/components/CardList.vue
@@ -1,23 +1,29 @@
 <script setup>
 import Card from './Card.vue'
 
+defineProps({
+  items: Array,
+  addToFavorite: Function
+})
+const emit = defineEmits(['addToFavorite'])
+
 const onClickAdd = () => {
-  alert('Добавить!')
+  emit('addToFavorite')
 }
 </script>
 
 <template>
   <div class="grid grid-cols-4 gap-5">
     <Card
-      title="Мужские кроссовки Nike Blazer Mid Suede"
-      imageUrl="/sneakers/sneakers-1.jpg"
-      :price="5000"
+      v-for="item in items"
+      :key="item.id"
+      :title="item.title"
+      :id="item.id"
+      :imageUrl="item.imageUrl"
+      :price="item.price"
       :onClickAdd="onClickAdd"
+      :onClickFavorite="() => emit('addToFavorite', item)"
+      :isFavorite="item.isFavorite"
     />
-    <Card />
-    <Card />
-    <Card />
-    <Card />
-    <Card />
   </div>
 </template>
