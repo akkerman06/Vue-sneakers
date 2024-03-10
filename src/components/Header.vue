44fc39ca0baab20e3590f999e53fa8539b637035
akkerman06 <islamhaluhaev225@gmail.com>
2024-03-10 15:56:12 +0300

    added vue-router auto-animate pages(home,favorites)

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -7,13 +7,15 @@ const emit = defineEmits(['openDrawer'])
 
 <template>
   <header class="flex justify-between border-b border-slate-200 px-10 py-8">
-    <div class="flex items-center gap-4">
-      <img src="/logo.png" alt="Logo" class="w-10" />
-      <div>
-        <h2 class="text-xl font-bold uppercase">Vue Sneakers</h2>
-        <p class="text-slate-400">Магазин лучших кроссовок</p>
-      </div>
-    </div>
+    <router-link to="/">
+      <div class="flex items-center gap-4">
+        <img src="/logo.png" alt="Logo" class="w-10" />
+        <div>
+          <h2 class="text-xl font-bold uppercase">Vue Sneakers</h2>
+          <p class="text-slate-400">Магазин лучших кроссовок</p>
+        </div>
+      </div></router-link
+    >
 
     <ul class="flex items-center gap-10">
       <li
@@ -24,10 +26,12 @@ const emit = defineEmits(['openDrawer'])
         <b>{{ totalPrice }} руб</b>
       </li>
 
-      <li class="flex items-center gap-3 text-slate-500 hover:text-black">
-        <img src="/heart.svg" alt="heart" />
-        <span>Закладки</span>
-      </li>
+      <router-link to="/favorites">
+        <li class="flex items-center gap-3 text-slate-500 hover:text-black">
+          <img src="/heart.svg" alt="heart" />
+          <span>Закладки</span>
+        </li></router-link
+      >
       <li class="flex items-center gap-3 text-slate-500 hover:text-black">
         <img src="/profile.svg" alt="profile" />
         <span>Профиль</span>
